4e0c9c1e149d71b380e23fb2a96b0345f719c079
Timothy Johnson <tim.johnson98@hotmail.com>
2019-04-23 06:40:44 +0000

    Header file

---
+++ Cyphers.h
@@ -0,0 +1,7 @@
+#ifndef CYPHERS_H_   /* Include guard */
+#define CYPHERS_H_
+
+void CaesarshiftEncrypt (char x[], int y);  /* An example function declaration */
+void CaesarshiftDecrypt (char x[], int y);
+
+#endif // CYPHERS_H_
