b1c57177688013cce207f16f18a5df623e6f3ee9
C3317428 <49180591+C3317428@users.noreply.github.com>
2019-04-23 16:46:19 +1000

    Delete Cyphers.h

--- Cyphers.h
+++
@@ -1,7 +0,0 @@
-#ifndef CYPHERS_H_   /* Include guard */
-#define CYPHERS_H_
-
-void CaesarshiftEncrypt (char x[], int y);  /* An example function declaration */
-void CaesarshiftDecrypt (char x[], int y);
-
-#endif // CYPHERS_H_
