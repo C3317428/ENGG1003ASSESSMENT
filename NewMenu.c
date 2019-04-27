1ad8adc26131a6c3c41244bb5eb1d7d4eb201b3a
C3317428 <49180591+C3317428@users.noreply.github.com>
2019-04-28 06:19:53 +1000

    Added File I/O properly

--- NewMenu.c
+++ NewMenu.c
@@ -3,35 +3,154 @@
 #include <stdlib.h>
 #include "Cyphers.h"
 
-int main0() {
-	char Filename[30]; // This is an array that will store the file name that the user wishes to run the program on.
-	static char String[1000]; // This is an array that will store the characters from the file.
+int main() {
+	printf("Hello there! \nWelcome to my encoder/decoder.\nIt reads text file in the folder this file \nis located in titled 'Input.txt'.\nThere is a limit of 2500 characters.\n\n");
 	
-	printf("Hello there! \nWelcome to my encoder/decoder. \nPlease enter the name of the file to be read to continue.");
+	/* scanf("%[^\n]%*c", Filename); This takes key inputs from the user and stores them as a string in 'a'. Stops taking inputs when the enter key is pressed. 
+	Not needed anymore but I thought it was really cool so I'm going to keep it here in a comment */
 	
-	printf("\nEnter here: ");	
-	scanf("%[^\n]%*c", Filename);  // This takes key inputs from the user and stores them as a string in 'a'. Stops taking inputs when the enter key is pressed.
-	
-	FILE *input;
- 	input = fopen(Filename, "r"); 
-        
-    if (input == NULL) {
-        printf("\nCan't open that input file! \nPlease check the name and try again.\n");
+	FILE *Input;
+ 	Input = fopen("Input.txt", "r"); 
+    if (Input == NULL) {
+        printf("Can't open that input file! \nPlease check the name and try again.\n");
         return 0;
         }
     
-    int x = 0;
-    while (!feof(input)) {     
-        fscanf(input, "%c", &String[x]);
-        x++;
+    FILE *Output;
+    Output = fopen("Output.txt", "w");
+    
+    fseek (Input, 1, SEEK_SET);
+    
+    char Type = fgetc(Input);
+    int  ArrayP = 0;
+    int  RKey;
+    char SKey[26];
+    char Message [10000];
+    
+    
+    for (int x = 0; x < 2500; x++){ // This loop zeros the entire array
+        Message[x] = 0;
+    }
+    
+    switch (Type) {
+            case '1':
+                printf("Rotation Encrypt:\n");
+                fseek (Input, 6, SEEK_SET);
+                RKey = fgetc(Input);
         
-        if (feof(input) != 0){
-            String[x] = '\0';
-            }
-        
+                while (!feof(Input)) {     
+                    char LFC; //Letter for cypher
+                    fscanf(Input, "%c", &LFC);
+                    Message[ArrayP] = LFC;
+                    ArrayP++;
+                }
+                
+                RotationEncrypt(Message, RKey);
+                printf("%s", Message);
+            break;
+            
+            case '2':
+                printf("Rotation Decrypt:\n");
+                fseek (Input, 6, SEEK_SET);
+                RKey = fgetc(Input);
+            
+                while (!feof(Input)) {     
+                    char LFC;
+                    fscanf(Input, "%c", &LFC);
+                    Message[ArrayP] = LFC;
+                    ArrayP++;
+                }
+                
+                RotationDecrypt(Message, RKey);
+                printf("%s", Message);
+                fprintf(Output, "%s", Message);
+            break;
+            
+            case '3':
+                printf("Substitution Encrypt:\n");
+                for (int Indicator = 6; Indicator < 32; Indicator++){
+                    fseek (Input, Indicator, SEEK_SET);
+                    SKey[ArrayP] = fgetc(Input);
+                    ArrayP++;
+                }            
+                ArrayP = 0;
+
+                fseek (Input, 33, SEEK_SET);
+                while (!feof(Input)) {     
+                    char LFC;
+                    fscanf(Input, "%c", &LFC);
+                    Message[ArrayP] = LFC;
+                    ArrayP++;
+                }
+                
+                SubstitutionEncrypt(Message, SKey);
+                printf("%s", Message);
+                fprintf(Output, "%s", Message);
+            break;
+           
+            case '4':
+                printf("Substitution Decrypt:\n");
+                for (int Indicator = 6; Indicator < 32; Indicator++){
+                    fseek (Input, Indicator, SEEK_SET);
+                    SKey[ArrayP] = fgetc(Input);
+                    ArrayP++;
+                }            
+                ArrayP = 0;
+                
+                fseek (Input, 33, SEEK_SET);
+                while (!feof(Input)) {     
+                    char LFC;
+                    fscanf(Input, "%c", &LFC);
+                    Message[ArrayP] = LFC;
+                    ArrayP++;
+                }
+                
+                SubstitutionDecrypt(Message, SKey);
+                printf("%s", SKey);
+                printf("%s", Message);
+                fprintf(Output, "%s", Message);
+            break;
+            
+            case '5':
+                printf("Rotation Crack:\n");            
+                fseek (Input, 6, SEEK_SET);
+                        
+                while (!feof(Input)) {     
+                    char LFC;
+                    fscanf(Input, "%c", &LFC);
+                    Message[ArrayP] = LFC;
+                    ArrayP++;
+                }
+                
+                //RotationCrack(Message);
+                printf("%s", Message);
+                fprintf(Output, "%s", Message);
+            break;
+            
+            case '6':
+                printf("Substitution Crack:\n");
+                fseek (Input, 6, SEEK_SET);
+                        
+                while (!feof(Input)) {     
+                    char LFC;
+                    fscanf(Input, "%c", &LFC);
+                    Message[ArrayP] = LFC;
+                    ArrayP++;
+                }
+                
+                //SubstitutionCrack(Message);
+                printf("%s", Message);
+                fprintf(Output, "%s", Message);
+            break;
+            
+            default:
+            printf("Something went wrong!");
         }
-	    printf("%s", String);
-	
+    printf("\n");
+    
+    
+    fseek (Input, 0, SEEK_SET);
+    
+    
 	return 0;
 }
-/*printf("Option 1: Caesar Encrypt \nOption 2: Caesar Decrypt \nOption 3: Substitution Encrypt \nOption 4: Substitution Decrypt\n");*/
