c8e1f019ecb61498be8c7974c146ea35ddb040bc
C3317428 <49180591+C3317428@users.noreply.github.com>
2019-04-28 02:46:04 +1000

    Update Cyphers.h

--- Cyphers.h
+++ Cyphers.h
@@ -4,10 +4,10 @@
 /* A function need to be declared before it can be used, otherwise the compiler will not know how to treat the function when it gets to it.
    Note: The function does not need to be defined here, only declared. */
 
-void RotationEncrypt (char x[], int y);  /* A function declaration for the function 'RotationEncrypt()' which returns nothing and accepts a character array and an integer as its arguments */
-void RotationDecrypt (char x[], int y);  /* A function declaration for the function 'RotationDecrypt()' which returns nothing and accepts a character array and an integer as its arguments */
-void SubstitutionEncrypt (char x[], char y[]);  /* A function declaration for the function 'SubstitutionEncrypt()' which returns nothing and accepts two character arrays as its arguments */
-void SubstitutionEncrypt (char x[], char y[]);  /* A function declaration for the function 'SubstitutionDecrypt()' which returns nothing and accepts two character arrays as its arguments */
+void RotationEncrypt (char x[], int Key);  /* A function declaration for the function 'RotationEncrypt()' which returns nothing and accepts a character array and an integer as its arguments */
+void RotationDecrypt (char x[], int Key);  /* A function declaration for the function 'RotationDecrypt()' which returns nothing and accepts a character array and an integer as its arguments */
+void SubstitutionEncrypt (char x[], char Key[]);  /* A function declaration for the function 'SubstitutionEncrypt()' which returns nothing and accepts two character arrays as its arguments */
+void SubstitutionDecrypt (char x[], char Key[]);  /* A function declaration for the function 'SubstitutionDecrypt()' which returns nothing and accepts two character arrays as its arguments */
 void RotationCrack (char x[]);  /* A function declaration for the function 'RotationCrack()' which returns nothing and accepts a character array as its argument */
 void SubstitutionCrack (char x[]);  /* A function declaration for the function 'SubstitutionCrack()' which returns nothing and accepts a character array as its argument */
 
