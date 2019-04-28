dd8d94eb14a01a3d5ccf98a8c79af1b86ca03c4f
C3317428 <49180591+C3317428@users.noreply.github.com>
2019-04-28 22:49:54 +1000

    Formatting

--- Menu.c
+++ Menu.c
@@ -34,10 +34,10 @@ int main() {
     
     switch (Type) {   // This checks the character that was the 2nd character in the input file and enacts the corresponding task
             
-            case '1':                             // If Type is '1'
-                printf("Rotation Encrypt:\n");    // Prints the name of the task to the sceen followed by a new line
-                fseek (Input, 6, SEEK_SET);       // Sets the cursor in the input file to in front of the 1st key digit
-                RKeyD1 = (int) fgetc(Input) - 48; // Sets RKeyD1 to the interger corresponding to the character read from the file (i.e. "2" becomes 2)
+        case '1':                             // If Type is '1'
+            printf("Rotation Encrypt:\n");    // Prints the name of the task to the sceen followed by a new line
+            fseek (Input, 6, SEEK_SET);       // Sets the cursor in the input file to in front of the 1st key digit
+            RKeyD1 = (int) fgetc(Input) - 48; // Sets RKeyD1 to the interger corresponding to the character read from the file (i.e. "2" becomes 2)
                 if (RKeyD1 == 0 || RKeyD1 == 1 || RKeyD1 == 2){ // If the first digit of the key is 0, 1, or 2
                     
                     fseek (Input, 7, SEEK_SET);       // Move the cursor in the input file to in front of the 2nd key digit
@@ -51,25 +51,26 @@ int main() {
                         RKeyT = RKeyD1 + RKeyD2;      // RKeyT equals RKeyD1 (tens) + RKeyD2 (units)
                     }
                 } 
+                
                 else {                                // If the first digit is not 0, 1, or 2
                     RKeyT = RKeyD1;                   // The actual value of the rotation key is just RKeyD1 with no weighting
                 }
                    
-                while (!feof(Input)) {                      // While not at the end of the file
-                    fscanf(Input, "%c", &Message[ArrayP]);  // Scans a character from the input file and enters it into the array 'Message' at element 'ArrayP'
-                    ArrayP++;                               // Increments ArrayP by 1, moving the array 'Message' onto the next element
-                }
+            while (!feof(Input)) {                      // While not at the end of the file
+                fscanf(Input, "%c", &Message[ArrayP]);  // Scans a character from the input file and enters it into the array 'Message' at element 'ArrayP'
+                ArrayP++;                               // Increments ArrayP by 1, moving the array 'Message' onto the next element
+            }
                 
-                RotationEncrypt(Message, RKeyT); // Passes the array 'Message' and the integer RKeyT to the function RotationEncrypt
-                printf("%s", Message);           // Prints the encrypted Message to the screen
-                fprintf(Output, "%s", Message);  // Writes the encrypted message to a text file called 'Output'
-            break;                               // Ends the switch case
+            RotationEncrypt(Message, RKeyT); // Passes the array 'Message' and the integer RKeyT to the function RotationEncrypt
+            printf("%s", Message);           // Prints the encrypted Message to the screen
+            fprintf(Output, "%s", Message);  // Writes the encrypted message to a text file called 'Output'
+        break;                               // Ends the switch case
             
             
-            case '2':                             // If Type is '2'
-                printf("Rotation Decrypt:\n");    // Prints the name of the task to the sceen followed by a new line
-                fseek (Input, 6, SEEK_SET);       // Sets the cursor in the input file to in front of the 1st key digit
-                RKeyD1 = (int) fgetc(Input) - 48; // Sets RKeyD1 to the interger corresponding to the character read from the file (i.e. "2" becomes 2)
+        case '2':                             // If Type is '2'
+            printf("Rotation Decrypt:\n");    // Prints the name of the task to the sceen followed by a new line
+            fseek (Input, 6, SEEK_SET);       // Sets the cursor in the input file to in front of the 1st key digit
+            RKeyD1 = (int) fgetc(Input) - 48; // Sets RKeyD1 to the interger corresponding to the character read from the file (i.e. "2" becomes 2)
                 if (RKeyD1 == 0 || RKeyD1 == 1 || RKeyD1 == 2){ // If the first digit of the key is 0, 1, or 2
                     
                     fseek (Input, 7, SEEK_SET);       // Move the cursor in the input file to in front of the 2nd key digit
@@ -83,95 +84,95 @@ int main() {
                         RKeyT = RKeyD1 + RKeyD2;      // RKeyT equals RKeyD1 (tens) + RKeyD2 (units)
                     }
                 } 
+                
                 else {                                // If the first digit is not 0, 1, or 2
                     RKeyT = RKeyD1;                   // The actual value of the rotation key is just RKeyD1 with no weighting
                 }
                    
-                while (!feof(Input)) {                      // While not at the end of the file
-                    fscanf(Input, "%c", &Message[ArrayP]);  // Scans a character from the input file and enters it into the array 'Message' at element 'ArrayP'
-                    ArrayP++;                               // Increments ArrayP by 1, moving the array 'Message' onto the next element
-                }
-                
-                
-                RotationDecrypt(Message, RKeyT); // Passes the array 'Message' and the integer RKeyT to the function RotationDecrypt
-                printf("%s", Message);           // Prints the encrypted Message to the screen
-                fprintf(Output, "%s", Message);  // Writes the encrypted message to a text file called 'Output'
-            break;                               // Ends the switch case
+            while (!feof(Input)) {                      // While not at the end of the file
+                fscanf(Input, "%c", &Message[ArrayP]);  // Scans a character from the input file and enters it into the array 'Message' at element 'ArrayP'
+                ArrayP++;                               // Increments ArrayP by 1, moving the array 'Message' onto the next element
+            }
+                               
+            RotationDecrypt(Message, RKeyT); // Passes the array 'Message' and the integer RKeyT to the function RotationDecrypt
+            printf("%s", Message);           // Prints the encrypted Message to the screen
+            fprintf(Output, "%s", Message);  // Writes the encrypted message to a text file called 'Output'
+        break;                               // Ends the switch case
             
             
-            case '3':                                                 // If Type is '3'
-                printf("Substitution Encrypt:\n");                    // Prints the name of the task to the sceen followed by a new line
+        case '3':                                                     // If Type is '3'
+            printf("Substitution Encrypt:\n");                        // Prints the name of the task to the sceen followed by a new line
                 for (int Indicator = 6; Indicator < 32; Indicator++){ // For cursor locations 6 to 31
                     fseek (Input, Indicator, SEEK_SET);               // Read from the Input file at the current cursor location
                     SKey[ArrayP] = fgetc(Input);                      // Put the character read into the corresponding element of the SubstitutionKey array
                     ArrayP++;                                         // Increment ArrayP by 1 to move on to the next element of the array
                 }            
-                ArrayP = 0;                                           // Resets ArrayP to 0
+            ArrayP = 0;                                               // Resets ArrayP to 0
 
-                fseek (Input, 33, SEEK_SET);                // Sets the cursor in the input file to in front of the encrypted letters
-                while (!feof(Input)) {                      // While not at the end of the file
-                    fscanf(Input, "%c", &Message[ArrayP]);  // Scans a character from the input file and enters it into the array 'Message' at element 'ArrayP'
-                    ArrayP++;                               // Increments ArrayP by 1, moving the array 'Message' onto the next element
+            fseek (Input, 33, SEEK_SET);                   // Sets the cursor in the input file to in front of the encrypted letters
+                while (!feof(Input)) {                     // While not at the end of the file
+                    fscanf(Input, "%c", &Message[ArrayP]); // Scans a character from the input file and enters it into the array 'Message' at element 'ArrayP'
+                    ArrayP++;                              // Increments ArrayP by 1, moving the array 'Message' onto the next element
                 }
                 
-                SubstitutionDecrypt(Message, SKey); // Passes the array 'Message' and the array 'SKey' to the function SubstitutionEncrypt
-                printf("%s", Message);              // Prints the encrypted Message to the screen
-                fprintf(Output, "%s", Message);     // Writes the encrypted message to a text file called 'Output'
-            break;                                  // Ends the switch case
+            SubstitutionDecrypt(Message, SKey); // Passes the array 'Message' and the array 'SKey' to the function SubstitutionEncrypt
+            printf("%s", Message);              // Prints the encrypted Message to the screen
+            fprintf(Output, "%s", Message);     // Writes the encrypted message to a text file called 'Output'
+        break;                                  // Ends the switch case
             
             
-            case '4':                                                 // If Type is '4'
-                printf("Substitution Decrypt:\n");                    // Prints the name of the task to the sceen followed by a new line
+        case '4':                                                     // If Type is '4'
+            printf("Substitution Decrypt:\n");                        // Prints the name of the task to the sceen followed by a new line
                 for (int Indicator = 6; Indicator < 32; Indicator++){ // For cursor locations 6 to 31
                     fseek (Input, Indicator, SEEK_SET);               // Read from the Input file at the current cursor location
                     SKey[ArrayP] = fgetc(Input);                      // Put the character read into the corresponding element of the SubstitutionKey array
                     ArrayP++;                                         // Increment ArrayP by 1 to move on to the next element of the array
                 }            
-                ArrayP = 0;                                           // Resets ArrayP to 0
+            ArrayP = 0;                                               // Resets ArrayP to 0
                 
-                fseek (Input, 33, SEEK_SET);                // Sets the cursor in the input file to in front of the encrypted letters
-                while (!feof(Input)) {                      // While not at the end of the file
-                    fscanf(Input, "%c", &Message[ArrayP]);  // Scans a character from the input file and enters it into the array 'Message' at element 'ArrayP'
-                    ArrayP++;                               // Increments ArrayP by 1, moving the array 'Message' onto the next element
+            fseek (Input, 33, SEEK_SET);                   // Sets the cursor in the input file to in front of the encrypted letters
+                while (!feof(Input)) {                     // While not at the end of the file
+                    fscanf(Input, "%c", &Message[ArrayP]); // Scans a character from the input file and enters it into the array 'Message' at element 'ArrayP'
+                    ArrayP++;                              // Increments ArrayP by 1, moving the array 'Message' onto the next element
                 }
                 
-                SubstitutionDecrypt(Message, SKey); // Passes the array 'Message' and the array 'SKey' to the function SubstitutionDecrypt
-                printf("%s", Message);              // Prints the encrypted Message to the screen
-                fprintf(Output, "%s", Message);     // Writes the encrypted message to a text file called 'Output'
-            break;                                  // Ends the switch case
+            SubstitutionDecrypt(Message, SKey); // Passes the array 'Message' and the array 'SKey' to the function SubstitutionDecrypt
+            printf("%s", Message);              // Prints the encrypted Message to the screen
+            fprintf(Output, "%s", Message);     // Writes the encrypted message to a text file called 'Output'
+        break;                                  // Ends the switch case
             
-            case '5':                            // If Type is '5'
-                printf("Rotation Crack:\n");     // Prints the name of the task to the sceen followed by a new line            
-                fseek (Input, 6, SEEK_SET);      // Sets the cursor in the input file to in front of the encrypted letters
+        case '5':                            // If Type is '5'
+            printf("Rotation Crack:\n");     // Prints the name of the task to the sceen followed by a new line            
+            fseek (Input, 6, SEEK_SET);      // Sets the cursor in the input file to in front of the encrypted letters
                         
                 while (!feof(Input)) {                      // While not at the end of the file
                     fscanf(Input, "%c", &Message[ArrayP]);  // Scans a character from the input file and enters it into the array 'Message' at element 'ArrayP'
                     ArrayP++;                               // Increments ArrayP by 1, moving the array 'Message' onto the next element
                 }
                 
-                //RotationCrack(Message);        // Passes the array 'Message' to the function RotationCrack
-                printf("%s", Message);           // Prints the encrypted Message to the screen
-                fprintf(Output, "%s", Message);  // Writes the encrypted message to a text file called 'Output'
-            break;                               // Ends the switch case
+            //RotationCrack(Message);        // Passes the array 'Message' to the function RotationCrack
+            printf("%s", Message);           // Prints the encrypted Message to the screen
+            fprintf(Output, "%s", Message);  // Writes the encrypted message to a text file called 'Output'
+        break;                               // Ends the switch case
             
-            case '6':                            // If Type is '6'
-                printf("Substitution Crack:\n"); // Prints the name of the task to the sceen followed by a new line
-                fseek (Input, 6, SEEK_SET);      // Sets the cursor in the input file to in front of the encrypted letters 
+        case '6':                            // If Type is '6'
+            printf("Substitution Crack:\n"); // Prints the name of the task to the sceen followed by a new line
+            fseek (Input, 6, SEEK_SET);      // Sets the cursor in the input file to in front of the encrypted letters 
                         
                 while (!feof(Input)) {                      // While not at the end of the file
                     fscanf(Input, "%c", &Message[ArrayP]);  // Scans a character from the input file and enters it into the array 'Message' at element 'ArrayP'
                     ArrayP++;                               // Increments ArrayP by 1, moving the array 'Message' onto the next element
                 }
                 
-                //SubstitutionCrack(Message);    // Passes the array 'Message' to the function SubstitutionCrack
-                printf("%s", Message);           // Prints the encrypted Message to the screen
-                fprintf(Output, "%s", Message);  // Writes the encrypted message to a text file called 'Output'
-            break;                               // Ends the switch case
+            //SubstitutionCrack(Message);    // Passes the array 'Message' to the function SubstitutionCrack
+            printf("%s", Message);           // Prints the encrypted Message to the screen
+            fprintf(Output, "%s", Message);  // Writes the encrypted message to a text file called 'Output'
+        break;                               // Ends the switch case
             
-            default:
+        default:
             printf("Something went wrong with the task selection!"); // Prints an error message if the task choice fails
-        }
+    }
     printf("\n"); // Adds a new line at the end of the program for formatting purposes
  
-    return 0; // Ends the program
+return 0;     // Ends the program
 }
